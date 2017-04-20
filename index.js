d7e4728ab58e222049d78afddf5867f020461589
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-04-20 20:36:35 +1000

    Made basic, all is really needed

--- index.js
+++ index.js
@@ -4,21 +4,9 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-var loadingpath = 'landing/landing.html';
-var contentType = 'text/html';
-
-switch (loadingpath) {
-    case '.js':
-        contentType = 'text/javascript';
-        break;
-    case '.css':
-        contentType = 'text/css';
-        break;
-}
-
 //Landing Page
 app.get('/', function(req, res) {
-  fs.readFile(loadingpath,function (err, data){
+  fs.readFile('landing/landing.html',function (err, data){
         res.writeHead(200, {'Content-Length' : contentType});
         res.write(data);
         res.end();
