be3e16079632e2bd3519d289c9b04d4da2e8d098
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-04-20 20:04:49 +1000

    Added landing page

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
-console.log("Succ");
+console.log("Succ Load");
+
+
+var express = require('express');
+var app = express();
+
+//Landing Page
+app.set('port', (process.env.PORT || 8080))
+app.get('/', function(req, res) {
+  fs.readFile('landing/landing.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+});
+app.listen(app.get('port'));
---
+++ landing/js/menu/control.Saves.js
@@ -0,0 +1,73 @@
+
+save = new WorkshopFiles();
+
+function ControllerSaves($scope, $rootScope, $location, $timeout, $routeParams)
+{
+	$rootScope.ShowBack = true;
+	Scope = $scope;
+
+	save.Init( 'ws_save', $scope, $rootScope );
+
+	Scope.Categories =
+	[
+		"trending",
+		"popular",
+		"latest"
+	];
+
+	Scope.SubCategories =
+	[
+		"scenes",
+		"machines",
+		"buildings",
+		"courses",
+		"others"
+	];
+
+	$scope.LoadSave = function( entry )
+	{
+		if ( !IN_ENGINE ) return;
+
+		if ( entry.local ) {
+			gmod.LoadSave( entry.info.file );
+			return;
+		}
+
+		//
+		// TODO: Some kind of `please wait` while we download 200kb
+		//
+
+		gmod.DownloadSave( entry.info.fileid );
+	}
+
+	$scope.DeleteLocal = function( entry )
+	{
+		if ( IN_ENGINE )
+		{
+			gmod.DeleteLocal( entry.info.file );
+			gmod.DeleteLocal( entry.info.background );
+		}
+
+		$scope.Switch( $scope.Category, $scope.Offset );
+	}
+
+	if ( IS_SPAWN_MENU )
+	{
+		if ( $routeParams.Category )
+		{
+			$timeout( function() { $scope.SwitchWithTag( $routeParams.Category, 0, $routeParams.Tag, $scope.MapName ); }, 100 );
+			return;
+		}
+
+		$timeout( function() { $scope.SwitchWithTag( 'trending', 0, $scope.MapName ); }, 100 );
+	}
+	else
+	{
+		$scope.Switch( 'local', 0 );
+	}
+}
+
+function OnGameSaved()
+{
+	Scope.Switch( 'local', 0 );
+}
