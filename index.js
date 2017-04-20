392d88f004b5fcb37ce8dca77f996d9cb37f4ca1
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-04-20 20:39:45 +1000

    was wondering why this wouldnt fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 //Landing Page
 app.get('/', function(req, res) {
   fs.readFile('landing/landing.html',function (err, data){
-        res.writeHead(200, {'Content-Length' : contentType});
+        res.writeHead(200, {'Content-Length' : ""});
         res.write(data);
         res.end();
     });
