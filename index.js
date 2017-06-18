f1787cad9ceca341d06378c8c0924aeaee9ee883
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-06-19 09:01:52 +1000

    temp diabled telegram message relay

--- index.js
+++ index.js
@@ -166,11 +166,11 @@ app.post('/api/telegramwebhook', function(req, res) {
 });
 
 //Event for messages in discord
-const TelegramBot = require('node-telegram-bot-api');
-const Tbot = new TelegramBot(process.env.telegramkey, {polling: true, parse_mode: "Markdown"});
-Dbot.on('message', function(user, userID, channelID, message, event){
-  Tbot.sendMessage(-112659114, "*"+user+"*: "+message);
-});
+//const TelegramBot = require('node-telegram-bot-api');
+//const Tbot = new TelegramBot(process.env.telegramkey, {polling: true, parse_mode: "Markdown"});
+//Dbot.on('message', function(user, userID, channelID, message, event){
+  //Tbot.sendMessage(-112659114, "*"+user+"*: "+message);
+//});
 
 //Listen
 app.listen((process.env.PORT || 8080));
