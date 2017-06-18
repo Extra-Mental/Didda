c9558799f06ad38f09ad9840f19d75759e0f1e4c
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-06-19 08:12:00 +1000

    fix pasring

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.post('/api/telegramwebhook', function(req, res) {
 
 //Event for messages in discord
 const TelegramBot = require('node-telegram-bot-api');
-const Tbot = new TelegramBot(process.env.telegramkey, {polling: true});
+const Tbot = new TelegramBot(process.env.telegramkey, {polling: true, parse_mode: "markdown"});
 Dbot.on('message', function(user, userID, channelID, message, event){
   Tbot.sendMessage(-112659114, "*"+user+"*: "+message);
 });
