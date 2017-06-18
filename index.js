3249677221d11df5ced7524083d5511955ce35c9
Extra-Mental <3xtra.m3ntal@gmail.com>
2017-06-18 22:09:27 +1000

    escape chars fix

--- index.js
+++ index.js
@@ -170,9 +170,8 @@ bot.on('message', function(user, userID, channelID, message, event){
 
   return http.get({
         host: "https://api.telegram.org",
-        path: "/bot"+process.env.telegramkey+"/sendmessage?chat_id=-112659114&text="+user+": "+message,
+        path: "/bot"+process.env.telegramkey+"/sendmessage?chat_id=-112659114&text="+ecodeURI(user)+": "+ecodeURI(message),
         agent: false,
-        port: 80
     }, function(response) {
 
     });
